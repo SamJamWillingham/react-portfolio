894b83ff5e197a9645ed63a7352937a2b5db8699
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-08-28 15:30:44 -0400

    improved security of links and added Footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="Container">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E03AQGbsqEpJcP2oA/profile-displayphoto-shrink_200_200/0?e=1603929600&v=beta&t=H6vTZMVntY6X1oRVNacPKGbhZxFguygFZX9cxum0Vuo"
+          width="140"
+          alt="Sam"
+          className="profile"
+        />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Sam Willingham</h1> <h3>social</h3>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <button className="linked-in">LinkedIn</button>
         </a>
-      </header>
+        <a
+          href="https://www.upwork.com/freelancers/~01f079375f21581fb6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="upWork">UpWork</button>
+        </a>
+        <a
+          href="https://github.com/SamJamWillingham"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="gitHub">GitHub</button>
+        </a>
+        <h3>projects</h3>
+        <a
+          href="https://gifted-tereshkova-4aa25e.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="weatherApp">Vanilla JS Weather App</button>
+        </a>
+        <a
+          href="https://ecstatic-leakey-e874b0.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="tarot">Tarot Card Drawer</button>
+        </a>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a
+          href="https://github.com/SamJamWillingham/portfolio-site"
+          className="footer"
+        >
+          Open course code
+        </a>
+      </small>
+    </div>
+  );
+}
