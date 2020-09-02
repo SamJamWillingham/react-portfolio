8c2207a1cb961a24f2d2347f7632f24fee35eef7
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-09-02 09:00:21 -0400

    added Bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Navbar.js";
 import Social from "./Social";
 import Projects from "./Projects";
 import Footer from "./Footer";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="Container">
+        <Navbar />
         <img
           src="https://media-exp1.licdn.com/dms/image/C4E03AQGbsqEpJcP2oA/profile-displayphoto-shrink_200_200/0?e=1603929600&v=beta&t=H6vTZMVntY6X1oRVNacPKGbhZxFguygFZX9cxum0Vuo"
           width="140"
